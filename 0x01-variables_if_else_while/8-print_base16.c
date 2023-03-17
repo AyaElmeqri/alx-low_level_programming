ebe93a650fda470539950634515f9a5b7cb81801
AyaElmeqri <ayaelmeqri@gmail.com>
2023-03-17 10:14:09 -0700

    Task8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,4 +1,13 @@
 #include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: print numbers of base16 in lowercase
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	int digit = 48; /*48; decimal rep of 0*/
